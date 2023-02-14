c46f9b75af26a521158018c882e84f8598964950
alecautry <alecautry@protonmail.com>
2023-02-14 12:01:04 -0600

    Added funcitons.py

---
+++ functions.py
@@ -0,0 +1,26 @@
+
+
+
+def AnalogFlightSensor(): 
+    
+    distance = 0.0
+
+    return distance
+
+def IRSensor(): # Read data for IRSensor
+    
+    IR_bool = False
+
+    return IR_bool
+
+def PhotoSensor(): # Read data from Photosensor
+
+    LightLevel = True
+
+    return LightLevel
+
+def LEDon(LED_Num):  # pass in a LED location and it will turn on
+    LED = LED_Num
+    print(LED + " is shining brightly")
+
+
